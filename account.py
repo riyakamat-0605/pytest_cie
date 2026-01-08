83dcb18cb7fe4d294a2b01a9ab97c577f3263a33
riyakamat-0605 <riyakamat2006@gmail.com>
2026-01-08 15:14:42 +0530

    Update account.py

--- account.py
+++ account.py
@@ -1,8 +1,8 @@
 def Account_details(acc_no, acc_holder_name, acc_type, balance):
     result = (
-        f"Account Number: {acc_no}\n"
+        f"Account Number: {acc_no}"
         f"Account Holder Name: {acc_holder_name}\n"
-        f"Account Type : {acc_type}\n"
+        f"Account Type: {acc_type}\n"
         f"Balance: {balance}"
     )
     return result
